827aa9b4378f7cccc014a06c1523000bd1dbc177
etefiok <diligentsamuel@gmail.com>
2023-10-20 13:41:18 -0700

    all new files

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// l=20, b=30
+// function add(l,b) {
+//     return l*b
+// }
+// var answer = add(20 , 30);
+// console.log(Result)
+
+// template literal
+var a= "Samuel";
+var b= 19;
+var c= " "
+
+var statement= `my name is ${a} i am ${b} years old;`
+console.log(statement)
